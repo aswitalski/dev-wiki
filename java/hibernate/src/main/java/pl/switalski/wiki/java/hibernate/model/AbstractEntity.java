d9353a6fc0feaabd1129e3e48d258bb70fff32b1
aswitalski <aleksander.switalski@gmail.com>
2013-07-20 00:07:55 +0200

    Added AbstractEntity class

---
+++ java/hibernate/src/main/java/pl/switalski/wiki/java/hibernate/model/AbstractEntity.java
@@ -0,0 +1,41 @@
+package pl.switalski.wiki.java.hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+@MappedSuperclass
+public class AbstractEntity {
+	
+	@Id
+	@Column(name = "id", length = 6, nullable = false)
+	protected int id;
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
+	
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+}
